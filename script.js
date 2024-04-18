1bc1d90c0a9ac5dee29badfc25665fa486d97d07
pratiyankkumar <kumarpratiyank@gmail.com>
2024-04-18 13:57:40 +0530

    Added a locastorage feature

--- script.js
+++ script.js
@@ -1,13 +1,19 @@
-
-
 let calculation='';
+
+calculation = localStorage.getItem('result1') || '';
+document.querySelector('.js-result-para')
+    .innerHTML = calculation;
+
+
 function calculateResult(num) {
+  calculation+=num;
   document.querySelector('.js-result-para')
-    .innerHTML = calculation+=num;
+    .innerHTML = calculation;
 }
 
 function resultShower() {
   calculation = eval(calculation);
   document.querySelector('.js-result-para')
     .innerHTML = calculation;
+  localStorage.setItem('result1', JSON.stringify(calculation));
 }
